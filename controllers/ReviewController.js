28089aba75d4162903c305c27980bb3509a08dbd
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2023-04-03 04:34:02 +0500

    Update ReviewController.js

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -6,53 +6,46 @@ import User from "../models/Users.js";
 // @access  Private
 
 export const createReview = async (req, res) => {
-    const { review, rating, user ,ratedBy } = req.body;
+  const { review, rating, user, ratedBy } = req.body;
 
-    try {
-        const newReview = await Review.create({
-            review,
-            rating,
-            user,
-            ratedBy: ratedBy,
-        });
-        if (newReview) {
-            const user = await User.findOneAndUpdate(
-                { _id: user },
-                { $push: { reviews: newReview._id } },
-                { new: true }
-            );
-            if (user) {
-                res.status(200).json({ newReview });
-            }
-        }
-    } catch (error) {
-        res.status(400).json({
-            message: error.message,
-            success: false,
-        });
+  try {
+    const newReview = await Review.create({
+      review,
+      rating,
+      user,
+      ratedBy: ratedBy,
+    });
+    if (newReview) {
+      const newUser = await User.findOneAndUpdate(
+        { _id: user },
+        { $push: { reviews: newReview._id } },
+        { new: true }
+      );
+      if (newUser) {
+        res.status(200).json({ newReview });
+      }
     }
-
-}
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
 
 export const getReviews = async (req, res) => {
+  //get  id
+  const { user } = req.params;
 
-    //get  id
-    const { user } = req.params;
-
-
-    try {
-        const reviews = await Review.find({ user: user }).populate("user");
-        if (reviews) {
-            res.status(200).json({ reviews });
-        }
+  try {
+    const reviews = await Review.find({ user: user }).populate("user");
+    if (reviews) {
+      res.status(200).json({ reviews });
     }
-    catch (error) {
-        res.status(400).json({
-            message: error.message,
-            success: false,
-        });
-    }
-}
-
-
-
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
